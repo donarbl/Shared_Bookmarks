e7022ff30fd6fe3a3e8867eb016e88555dda7be8
donarbl <donarablanc@gmail.com>
2025-02-19 17:19:40 +0000

    test cases

---
+++ test.script.js
@@ -0,0 +1,12 @@
+import {
+  handleUserChange,
+  displayBookmarks,
+  createBookmarkElement,
+  handleBookmarkSubmit,
+} from "./script.js";
+
+describe ("handleBookmarkSubmit", () =>{
+  it ("should add a new bookmark", ()=>{
+    
+  })
+})
